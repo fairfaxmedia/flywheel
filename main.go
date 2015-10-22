b28a513729e76502b16e92847582d1dbeb9b4054
David Baggerman <david.baggerman@fairfaxmedia.com.au>
2015-10-22 14:09:46 +1100

    Allow storing status to file
    Allows preserving LastStarted/LastStopped across restarts

--- main.go
+++ main.go
@@ -1,10 +1,73 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+func readStatusFile(statusFile string) *Pong {
+	fd, err := os.Open(statusFile)
+	if err != nil {
+		if err != os.ErrNotExist {
+			log.Printf("Unable to load status file: %v", err)
+		}
+		return nil
+	}
+
+	stat, err := fd.Stat()
+	if err != nil {
+		log.Printf("Unable to load status file: %v", err)
+		return nil
+	}
+
+	buf := make([]byte, int(stat.Size()))
+	_, err = io.ReadFull(fd, buf)
+	if err != nil {
+		log.Printf("Unable to load status file: %v", err)
+		return nil
+	}
+
+	var status Pong
+	err = json.Unmarshal(buf, &status)
+	if err != nil {
+		log.Printf("Unable to load status file: %v", err)
+		return nil
+	}
+
+	return &status
+}
+
+func writeStatusFile(statusFile string, flywheel *Flywheel) {
+	var pong Pong
+
+	fd, err := os.OpenFile(statusFile, os.O_CREATE | os.O_TRUNC | os.O_WRONLY, 0644)
+	if err != nil {
+		log.Print("Unable to write status file: %v", err)
+		return
+	}
+	defer fd.Close()
+
+	pong.Status = flywheel.status
+	pong.StatusName = StatusString(flywheel.status)
+	pong.LastStarted = flywheel.lastStarted
+	pong.LastStopped = flywheel.lastStopped
+
+	buf, err := json.Marshal(pong)
+	if err != nil {
+		log.Print("Unable to write status file: %v", err)
+		return
+	}
+
+	_, err = fd.Write(buf)
+	if err != nil {
+		log.Print("Unable to write status file: %v", err)
+		return
+	}
+}
 
 func main() {
 	var err error
@@ -12,9 +75,11 @@ func main() {
 
 	var listen string
 	var configFile string
+	var statusFile string
 
 	flag.StringVar(&listen, "listen", "0.0.0.0:80", "Address and port to listen on")
 	flag.StringVar(&configFile, "config", "", "Config file to read settings from")
+	flag.StringVar(&statusFile, "status-file", "", "File to save runtime status to")
 	flag.Parse()
 
 	if configFile != "" {
@@ -25,6 +90,16 @@ func main() {
 	}
 
 	flywheel := New(config)
+
+	if statusFile != "" {
+		status := readStatusFile(statusFile)
+		if status != nil {
+			flywheel.status = status.Status
+			flywheel.lastStarted = status.LastStarted
+			flywheel.lastStopped = status.LastStopped
+		}
+		defer writeStatusFile(statusFile, flywheel)
+	}
 
 	go flywheel.Spin()
 

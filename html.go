67d70e59731eb6474d293836eef7170b379bdf0a
David Baggerman <david.baggerman@fairfaxmedia.com.au>
2015-11-02 13:29:47 +1100

    Tweak HTML

--- html.go
+++ html.go
@@ -6,8 +6,8 @@ This could probably be smarter, but it'll do for now.
 
 const HTML_STOPPED = `
 	<html>
-		<body>
-			<p>The instances for this service are currently powered down.</p>
+		<body style="color: #333333; background: #f5f5f5;">
+			<h1 style="text-align: center; margin-top: 50px; font-size: larger;">Your service is currently powered down</h1>
 			<p><a href="%s">Click here</a> to start.</p>
 		</body>
 	</html>`
@@ -19,8 +19,9 @@ const HTML_STARTING = `
 				window.location.reload(1);
 			}, 5000);
 		</script>
-		<body>
-			<p>Your service is starting, please wait.</p>
+		<body style="color: #333333; background: #f5f5f5">
+			<h1 style="text-align: center; margin-top: 50px; font-size: larger;">Your service is starting, please wait.</h1>
+			<p>Your site will be loaded once startup is complete.</p>
 		</body>
 	</html>`
 
@@ -31,21 +32,24 @@ const HTML_STOPPING = `
 				window.location.reload(1);
 			}, 5000);
 		</script>
-		<body>
-			<p>The instances for this service are being powered down.</p>
+		<body style="color: #333333; background: #f5f5f5">
+			<h1 style="text-align: center; margin-top: 50px; font-size: larger;">Your service is being powered down.</h1>
+			<p>Please wait for shutdown to complete before restarting.</p>
 		</body>
 	</html>`
 
 const HTML_UNHEALTHY = `
 	<html>
-		<body>
-			<p>The instances for this service appear to be in an unhealthy or inconsistent state.</p>
+		<body style="color: #333333; background: #f5f5f5">
+			<h1 style="text-align: center; margin-top: 50px; font-size: larger;">Your service appears to be in an unhealthy or inconsistent state</h1>
+			<p>This may be a temporary error, or may require manual intervention.</p>
 		</body>
 	</html>`
 
 const HTML_ERROR = `
 	<html>
-		<body>
-			<p>An error occured processing your request: %v</p>
+		<body style="color: #333333; background: #f5f5f5">
+			<h1 style="text-align: center; margin-top: 50px; font-size: larger;">An error occured processing your request</h1>
+			<p>%v</p>
 		</body>
 	</html>`

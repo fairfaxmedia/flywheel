d04eba6bb6089845cbf4be663f8c810d17995c7a
Dejan Golja <dejan.golja@fairfaxmedia.com.au>
2016-07-30 01:05:58 +1000

    - add version flag - add Makefile

---
+++ version.go
@@ -0,0 +1,39 @@
+package flywheel
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// The git commit that was compiled. This will be filled in by the compiler.
+var GitCommit string
+
+// Version shoul be overwritten via Makefile
+const Version = "0.2.0"
+
+// VersionInfo stores version and GitCommit SHA
+type VersionInfo struct {
+	Revision string
+	Version  string
+}
+
+// GetVersion retrieve version
+func GetVersion() *VersionInfo {
+
+	return &VersionInfo{
+		Revision: GitCommit,
+		Version:  Version,
+	}
+}
+
+func (c *VersionInfo) String() string {
+	var versionString bytes.Buffer
+
+	fmt.Fprintf(&versionString, "Flywheel v%s", c.Version)
+
+	if c.Revision != "" {
+		fmt.Fprintf(&versionString, " (%s)", c.Revision)
+	}
+
+	return versionString.String()
+}
